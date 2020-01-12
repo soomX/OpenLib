275ff70964deacc4f75376955e7eff21d15ff181
soomX <59781791+soomX@users.noreply.github.com>
2020-01-11 19:28:47 -0600

    added border simulator
    not many functions as of right now, hopefully later ill get more working.

---
+++ main.lua
@@ -0,0 +1,16 @@
+Border Simulator:
+loadstring(game:HttpGet("https://raw.githubusercontent.com/soomX/OpenLib/master/library/bordersimulator.lua", true))()
+--[[
+	border simulator functions
+--]]
+meds(true) -- asks if the script will be executed in a loop required to enter true or false.
+speed(true) --  asks if the script will be executed in a loop required to enter true or false.
+--tp's below
+-- the names of the ones below explain themselves
+medicspawn()
+rebelspawn()
+safespot()
+clothes()
+bankmain()
+banklasers1()
+bankvault()
