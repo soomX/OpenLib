8c4dcfffdb74b3d510d8d1a5aa4117eccc196d49
soomX <59781791+soomX@users.noreply.github.com>
2020-01-11 19:38:20 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,8 @@
-Border Simulator:
-loadstring(game:HttpGet("https://raw.githubusercontent.com/soomX/OpenLib/master/library/bordersimulator.lua", true))()
 --[[
 	border simulator functions
+notice: for the meds i suggest messing with the time as it plays this sound when activated, im trying to see if its removable.
 --]]
-meds(true) -- asks if the script will be executed in a loop required to enter true or false.
+meds(true, 0.1) -- asks if the script will be executed in a loop required to enter true or false. then for the amount of time you want for it to execute the loop
 speed(true) --  asks if the script will be executed in a loop required to enter true or false.
 --tp's below
 -- the names of the ones below explain themselves
